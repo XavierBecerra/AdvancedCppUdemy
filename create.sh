17213bfa19f511ee6acd1c829571291735891b0b
xBecerra <xavi@debian>
2018-05-16 17:59:21 +0200

    adding files to autogenerate projects folders

---
+++ create.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Script to create a specific-directory on all subfolders in a parent-directory
+
+mkdir "$1" "$1/src" "$1/build" 
+cp "BasicCMakeLists.txt" "$1/CMakeLists.txt"
+cp "Basic.cpp" "$1/src/$2"
